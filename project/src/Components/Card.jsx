cbeac33a8d5d1ffb42a9cb3279fe01711912803b
Vikram <vikramdev772@gmail.com>
2023-06-03 15:55:33 +0530

    animation

---
+++ project/src/Components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div>
+      <div className="card">
+        Tranform
+      </div>
+      
+        
+      
+    </div>
+  )
+}
+
+export default Card
